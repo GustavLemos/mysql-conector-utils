1e4ec3e0023d8deadd18e54b8ad94e97a1b0d44c
Gustavo Lemos dos Santos <57879409+GustavLemos@users.noreply.github.com>
2022-10-15 00:51:04 -0300

    Create pyodbc-conector.py

---
+++ pyodbc-conector.py
@@ -0,0 +1,32 @@
+import pyodbc 
+import pandas 
+
+# strings of conection with sql
+server = ''
+data_base = ''
+user = ''
+password = ''
+
+cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="+ server +";DATABASE="+ data_base +";UID="+ user +";PWD="+ password)
+
+#string of table
+table = ''
+
+cursor = cnxn.cursor()
+cursor.execute('SELECT * FROM '+ table)
+
+#print of results of rows
+#for row in cursor:
+#    print('row = %r' % (row,))
+
+#capture rows whit comand transact sql
+rows = cursor.execute('SELECT * FROM '+ table).fetchall() 
+
+#Create dataframe
+df = pandas.DataFrame([tuple(t) for t in rows],columns=['id','tipo_jornalista','relevancia','codigo','data_vigencia_inicio','data_vigencia_final']) 
+print(df)
+
+#Save dataframe in csv
+df.to_csv('file.csv')
+Footer
+© 2022 GitHub, Inc.
